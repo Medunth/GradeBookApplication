6f676c829dc1570ef9c78ee875567f0304ecc91f
Artem <artem.babych1@microsoft.wsei.edu.pl>
2026-04-18 01:27:21 +0200

    Add GradeBookType enum

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,22 @@
+﻿namespace GradeBook.Enums
+{
+    public enum GradeBookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
+    }
+}
+namespace GradeBooks.Enums
+{
+    public enum GradeBookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
+    }
+}
